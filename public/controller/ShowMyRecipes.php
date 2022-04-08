efd092568b5f7d6d7734b9f51381b97a2edd20a5
AnthonyPayan <anthony.payan@outlook.fr>
2022-04-08 14:27:46 +0200

    Extraction traitement dans template

--- public/controller/ShowMyRecipes.php
+++ public/controller/ShowMyRecipes.php
@@ -5,11 +5,19 @@ session_start();
 $user_id = $_SESSION['id'];
 $category_id = $_GET['category_id'];
 
+
 if (empty($category_id)) {
    header("location :/profil.php?id=$user_id");
 } else {
    $recipes = selectByWaW($pdo, 'recipes', 'category_id', 'user_id', $category_id, $user_id);
 }
+
+
+$containerDisplay = "displaynone";
+if (empty($recipes)) {
+   $containerDisplay = "container-info";
+}
+
 
 $template = "../templates/show_my_recipes.php";
 include("../../profil_layout.php");
