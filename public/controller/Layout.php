6c43dc67e62b8fbfb624a8414160473ab578bc8a
AnthonyPayan <anthony.payan@outlook.fr>
2022-04-09 07:03:18 +0200

    Refactorisation traitement dans template

--- public/controller/Layout.php
+++ public/controller/Layout.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
 
+$classDisplaySession = "displaynone";
+$classDisplaySessionEmpty = NULL;
 // Traitement des notifications dans la navigation.
 if ($_SESSION) {
     $session_id = $_SESSION['id'];
     // $session_name = selectOneByFetch($pdo, 'nickname', 'users', 'id', $session_id);
     $attente = ATTENTE;
     $recipe_info = countWaWfetch($pdo, 'id', 'recipes_wait', 'recipes', 'user_id', 'category_id', $session_id, $attente);
+
+    $classDisplaySession = NULL;
+    $classDisplaySessionEmpty = "displaynone";
+
+    if (!empty($recipe_info['recipes_wait'])) {
+        $notifWait = '<span class="span-badge">' . $recipe_info["recipes_wait"] . '</span>';
+    } else {
+        $notifWait = NULL;
+    }
 }
