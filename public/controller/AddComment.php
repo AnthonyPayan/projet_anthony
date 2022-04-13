39de2d0e71c81802cc79e20377eb105ce3e9dc26
AnthonyPayan <anthony.payan@outlook.fr>
2022-04-13 20:39:25 +0200

    Add bindValue on Addrecipe + AddComment

--- public/controller/AddComment.php
+++ public/controller/AddComment.php
@@ -3,6 +3,7 @@ require('../../libraries/services/functions.php');
 
 
 if (isset($_POST)) {
+
 	$comment = htmlentities($_POST['comment']);
 	$ranked = intval($_POST['ranked']);
 	$user_id = htmlentities($_POST['user']);
@@ -10,16 +11,21 @@ if (isset($_POST)) {
 
 	$sql = "INSERT INTO comments (comment, ranked, user_id, recipe_id)
 			VALUES (?, ?, ?, ?)";
-	$pdo->bindValue(1, PDO::PARAM_STR);
-	$pdo->bindValue(2, PDO::PARAM_STR);
-	$pdo->bindValue(3, PDO::PARAM_STR);
-	$pdo->bindValue(4, PDO::PARAM_STR);
 
 	$query = $pdo->prepare($sql);
+
+	$query->bindValue(1, PDO::PARAM_STR);
+	$query->bindValue(2, PDO::PARAM_STR);
+	$query->bindValue(3, PDO::PARAM_INT);
+	$query->bindValue(4, PDO::PARAM_STR);
+
 	$query->execute([$comment, $ranked, $user_id, $recipe_id]);
+
+	// [$comment, $ranked, $user_id, $recipe_id] ce trouve dans le execute
 
 	header("location: /public/controller/ShowComments.php?recipe_id=$recipe_id");
 	exit();
 } else {
+
 	header("location: Error.php?error=20");
 }
--- public/controller/AddRecipe.php
+++ public/controller/AddRecipe.php
@@ -3,6 +3,7 @@ require('../../libraries/services/functions.php');
 
 
 if (!empty($_POST)) {
+
 	move_uploaded_file($_FILES["image"]["tmp_name"], getcwd() . "/../src/img/" . $_FILES["image"]["name"]);
 	$title = htmlentities($_POST['title']);
 	$description = htmlentities($_POST['description']);
@@ -10,11 +11,19 @@ if (!empty($_POST)) {
 	$image = $_FILES['image']['name'];
 	$category_id = $_POST['category'];
 
-
 	$sql = "INSERT INTO recipes (title, description, image, user_id, category_id)
 		   VALUES (?, ?, ?, ?, ?)";
+
 	$query = $pdo->prepare($sql);
-	$query->execute([$title, $description, $image, $user_id, $category_id]);
+
+	$query->bindValue(1, $title, PDO::PARAM_STR);
+	$query->bindValue(2, $description, PDO::PARAM_STR);
+	$query->bindValue(3, $image, PDO::PARAM_STR);
+	$query->bindValue(4, $user_id, PDO::PARAM_INT);
+	$query->bindValue(5, $category_id, PDO::PARAM_STR);
+
+	$query->execute();
+
 	header("location: /index.php");
 	exit();
 }
