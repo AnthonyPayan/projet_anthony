dc49ba6b51c5305d32d821291bf8158c45153ae0
AnthonyPayan <anthony.payan@outlook.fr>
2022-04-14 17:46:42 +0200

    Add bindValue on UpdateRecipeCategory

--- public/controller/UpdateRecipeCategory.php
+++ public/controller/UpdateRecipeCategory.php
@@ -4,19 +4,27 @@ require('../../libraries/services/functions.php');
 
 if (!empty($_POST['recipe_id'])) {
 
-    $recipe_id = $_POST['recipe_id'];
-    $user_id = $_POST['user_id'];
-    $category_id = $_POST['category_id'];
+    $recipe_id = htmlentities($_POST['recipe_id']);
+    $user_id = htmlentities($_POST['user_id']);
+    $category_id = htmlentities($_POST['category_id']);
     $attente = ATTENTE;
 
 
-    $sql = "UPDATE recipes SET category_id = ? WHERE id = $recipe_id";
+    $sql = "UPDATE recipes SET category_id = :category_id WHERE id = :recipe_id";
     $query = $pdo->prepare($sql);
-    $query->execute([$category_id]);
+
+    $query->bindValue("category_id", $category_id);
+    $query->bindValue("recipe_id", $recipe_id);
+
+    $query->execute();
 
 
-    $sql = "SELECT id FROM recipes WHERE user_id = $user_id AND category_id = $attente";
+    $sql = "SELECT id FROM recipes WHERE user_id = :user_id AND category_id = :category_id";
     $query = $pdo->prepare($sql);
+
+    $query->bindValue("user_id", $user_id);
+    $query->bindvalue("category_id", $attente);
+
     $query->execute();
     $recipe_info = $query->fetchAll();
 
